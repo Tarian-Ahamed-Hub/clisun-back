93fc6521ec1cf1f71e915bd2587cb6789ae91f2a
Tarian Ahamed <tarian.ahamed6@gmail.com>
2025-01-11 20:03:44 +0600

    ready

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const router = express.Router();
 const app = express();
+process.env.TZ = 'Asia/Dhaka';
 const multer = require("multer") 
 // const cookieSession = require("cookie-session")
 const session = require("express-session");
